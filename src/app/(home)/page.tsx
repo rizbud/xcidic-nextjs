6bf14a73d109d125f9ff705838fbd3a048d38f5c
Rizki Budi <rizkibudi513@gmail.com>
2024-04-01 21:46:05 +0700

    feat: add header

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import { PokemonList } from "~/components";
 
 export const metadata: Metadata = {
@@ -9,7 +11,17 @@ export const metadata: Metadata = {
 export default async function Home() {
   return (
     <div className="w-full p-4 md:px-8 lg:px-16 bg-gradient-to-b from-emerald-700 to-indigo-800 min-h-screen">
-      <div className="mx-auto max-w-screen-xl">
+      <div className="mx-auto max-w-screen-xl flex flex-col gap-6">
+        <Link href="/">
+          <Image
+            src="/pokemon.webp"
+            alt="Pokemon Logo"
+            width={200}
+            height={200}
+            className="mx-auto"
+          />
+        </Link>
+
         <PokemonList />
       </div>
     </div>
